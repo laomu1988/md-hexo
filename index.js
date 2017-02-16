6e4425de8a8f1e3131c95871fb1406728d3d5606
muzhilong <laomu1988@qq.com>
2017-02-16 19:19:15 +0800

    顶部多加一行

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ function mdhexo(filepath) {
   tags = tags.length > 1 ? JSON.stringify(tags).replace(/"/g, '') : tags.join('');
   var lstatSync = fs.lstatSync(filepath);
   var t = time.format(lstatSync.birthtime, 'yyyy-LL-dd HH:mm:ss');
-  var added = 'title: ' + filename + '\n' + 'tags: ' + tags + '\ndate: ' + t + '\n---\n\n';
-  
+  var added = '---\ntitle: ' + filename + '\n' + 'tags: ' + tags + '\ndate: ' + t + '\n---\n\n';
+
   console.log('[hexo_sign] title:', filename, '   tags:', tags);
   fs.writeFileSync(filepath, added + source, 'utf8');
 }
 
 
-module.exports = function (folder, filterFn) {
+module.exports = function(folder, filterFn) {
   if (!filterFn) {
-    filterFn = function (file) {
+    filterFn = function(file) {
       // 忽略node_module目录下内容
       var relative = path.relative(main_place, file);
       return !/node_modules/.test(relative);
     }
   }
-  var files = filter.sync(folder, function (file) {
+  var files = filter.sync(folder, function(file) {
     return (!path.extname(file) || path.extname(file) === '.md') && filterFn(file);
   });
   if (!files || files.length == 0) return;
--- readme.md
+++ readme.md
@@ -8,8 +8,9 @@ $ npm install -g md-hexo
 ```
 
 ## usage
-run `mdhexo` in your project folder, and then the markdown files without sign `---` will add sign: 
+run `mdhexo` in your project folder, and then the markdown files without sign `---` will add sign:
 ```
+---
 title: filename
 tags: folder
 date: 2017-01-01 10:10:10
@@ -24,4 +25,5 @@ $ mdhexo [folder]
 ## todo
 * [x] add date sign from birthtime of file
 * [x] 忽略node_modules目录
+* [ ] 根据markdown内容中的#查找title
 * [ ] 补充缺失的sign
